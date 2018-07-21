866158973ba0cdb51e14057b9abeb9100591f9ab
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-21 14:51:28 -0700

    save an actor not working

---
+++ test/e2e/actor.test.js
@@ -0,0 +1,55 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+const { checkOk } = request;
+
+describe('Actors API', () => {
+
+    beforeEach(() => dropCollection('actors'));
+
+    function save(actor) {
+        return request
+            .post('/api/actors')
+            .send(actor)
+            .then(checkOk)
+            .then(({ body }) => body);
+    }
+
+    let winonaRyder;
+    let donJohnson;
+
+    // beforeEach(() => {
+    //     return request
+    //         .post('/api/actors')
+    //         .send({ name: 'Winona Ryder' })
+    //         .then(({ body }) => winonaRyder = body);
+    // });
+
+
+    beforeEach(() => {
+        return request({
+            name: 'Winona Ryder',
+            dob: new Date(1971, 9, 29),
+            pob: 'MN'
+        })
+            .then(data => {
+                winonaRyder = data;
+            });
+    });
+
+    beforeEach(() => {
+        return save({
+            name:'Don Johnson',
+            dob: new Date(1949, 11, 15),
+            pob: 'MO'
+        })
+            .then(data => {
+                donJohnson = data;
+            });
+    });
+
+    it('saves an actor', () => {
+        assert.isOk(winonaRyder._id);
+    });
+});
